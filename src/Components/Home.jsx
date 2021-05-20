b263c78140bfd051d0a08e02e9ab05b579d14756
marcosrodriguezsr <marcos.rodriguez@correo.unimet.com>
2021-05-20 15:42:21 -0400

    actualizar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,12 @@
-import Slideshow from './Components/Slideshow';
-
 export default function Home(){
     return(
        <>
 
-
            <main className="hero">
+      
+             
       <div className="info-content" data-aos="fade-up" data-aos-duration="2000">
+        
         <h3>Welcome To</h3>
         <h1>E-Vent</h1>
         <p>Crea tu propio evento desde casa a tu gusto, y nosotros hacemos el resto por ti</p>
