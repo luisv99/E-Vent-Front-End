b59ee419904c33fee27e5733d483c49f74767100
Gustavo Toro <gustavo@torocaballero.com>
2021-05-24 07:55:10 -0400

    Hace el login y sign up pero el mensaje de confirmacion no aparece y no te manda al profile

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ import {
   Link
 } from "react-router-dom";
 
+
 function App() {
+    
   return (
     <Router>
       <div className="App">
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,9 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './LoginStyles.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faBars,faShoppingCart} from '@fortawesome/free-solid-svg-icons'
-
+import Axios from 'axios';
 
 export default function Login(){
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    
+    const handleSubmit = event => {
+        event.preventDefault();
+        Axios.post("./api/auth/logIn", {
+            email: email,
+            password: password,
+        }).then((response) =>{
+            alert("Succesful LogIn");
+        },
+        (error) =>{
+            alert(error.response.data.message);
+        });
+    }
+
     return(
         <>
             <div className="signupSection">
@@ -14,16 +31,16 @@ export default function Login(){
                     <h1>LOGO</h1>
                 </div>
                 
-                <form action="#" method="POST" className="signupForm" name="signupform">
+                <form onSubmit={handleSubmit} method="POST" className="signupForm" name="signupform">
                     <h2>Iniciar Sesion</h2>
                     <ul className="noBullet">
                         <li>
                             <label htmlfor="email"></label>
-                            <input type="email" className="inputFields" id="email" name="email" placeholder="Email"  required />
+                            <input type="email" className="inputFields" id="email" name="email" placeholder="Email"  required onChange = {(e)=>{setEmail(e.target.value)}}/>
                         </li>
                         <li>
                             <label htmlfor="password"></label>
-                            <input type="password" className="inputFields" id="password" name="password" placeholder="Contraseña"  required />
+                            <input type="password" className="inputFields" id="password" name="password" placeholder="Contraseña"  required onChange = {(e)=>{setPassword(e.target.value)}}/>
                         </li>
                         <li id="center-btn">
                             <input type="submit" id="join-btn" name="join" alt="Join" value="Entrar a E-Vent"/>
