514d64ae2e894b72508c59dbda060fcae0b792bc
Luis Vargas <72893367+luisv99@users.noreply.github.com>
2021-05-28 11:44:29 -0400

    sigo avance del home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -19,7 +19,7 @@ export default function Home(){
        
        
         <main className="hero">
-          <div className="slide">
+          <div className="slide" data-aos="fade-up-right" data-aos-duration="2000">
             <Slideshow/>
           </div>
           
@@ -41,17 +41,36 @@ export default function Home(){
       
 
       <div className="hero2" >
-        <div className="content1" data-aos="fade-up">
-          <h2 className="title1">Unica agencia de festejos virtual <br/>en el pais</h2>
-          <img className="img1" src="/Imagenes/party2.png" alt="" />
+        <div className="content1">
+          <h2 className="title1" data-aos="fade-up-right" data-aos-duration="2000">Unica agencia de festejos <span style={{color: "#9c0525"}}>VIRTUAL</span> <br/>en el pais</h2>
+          <img data-aos="fade-up" data-aos-duration="1800" className="img1" src="/imagenes/party2.png" alt="" />
         </div>
       </div>
 
-      <h1 className="title2">Contrata tus servicios favoritos</h1>
+      <div className="title2">
+        <h1 data-aos="zoom-in" data-aos-duration="2000" >Contrata tus servicios favoritos</h1>
+      </div>
+      
       <div className="hero3">
         <div className="content2">
-        <img className="img2" src="/imagenes/SantaTeresa.jpg" alt="" />
-        <h2 className="title3" data-aos="fade-up">Bebidas</h2>
+        <img data-aos="fade-up" data-aos-duration="1800" className="img2" src="/imagenes/SantaTeresa.jpg" alt="" />
+        <h2 className="title3" data-aos="fade-up" data-aos-duration="1800">Bebidas</h2>
+        </div>
+      </div>
+
+      <div className="hero3">
+        <div className="content2">
+        <h2 className="title3" data-aos="fade-up" data-aos-duration="1800">Comida</h2>
+        <img data-aos="fade-up" data-aos-duration="1800" className="img2" src="/imagenes/Caracas-Catering.jpg" alt="" />
+        <img data-aos="fade-up" data-aos-duration="1800" className="img3" src="/imagenes/Arabito.png" alt="" />
+        </div>
+      </div>
+      
+
+      <div className="hero3">
+        <div className="content2">
+        <img data-aos="fade-up" data-aos-duration="1800" className="img3" src="/imagenes/party4.png" alt="" />
+        <h2 className="title4" data-aos="fade-up" data-aos-duration="1800">Fiestas</h2>
         </div>
       </div>
       </>
