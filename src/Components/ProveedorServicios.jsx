878b1597ec8d9b58381a1ef59fbdf323dc14ab8d
Barbara Alvarez <>
2021-06-09 12:25:50 -0400

    YA FUNCIONA

--- src/Components/ProveedorServicios.jsx
+++ src/Components/ProveedorServicios.jsx
@@ -17,7 +17,7 @@ export default function Admin(){
     }, []);
 
     const getServicios = () =>{
-        Axios.get("http://localhost:5000/").then((res)=>{
+        Axios.get("http://localhost:5000/services").then((res)=>{
             setServicios(res.data)
             console.log(res.data)
         }
