f37683e39e78d07a7f1ff1123f0d26d6eb99818b
marcosrodriguezsr <marcos.rodriguez@correo.unimet.com>
2021-06-14 18:24:05 -0400

    nada

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -29,6 +29,7 @@ export default function NavBar(){
                 <li><Link to="/Services">Servicios</Link></li>
                 <li><Link to="/Checkout">Checout</Link></li>
                 
+                
             </ul>
 
             <div className="menu-bar" onClick={()=>setOpen(prev=>!prev)} >
