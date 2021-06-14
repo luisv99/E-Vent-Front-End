32adaf79be44283848b04813daaf16af8b382f97
Luis Vargas <72893367+luisv99@users.noreply.github.com>
2021-06-14 15:44:57 -0400

    Luis

--- src/Components/Proveedores.jsx
+++ src/Components/Proveedores.jsx
@@ -18,17 +18,7 @@ export default function Admin(){
         searchProveedores()
     }, [proveedorByName]);
 
-
     
-    const getProveedores = () =>{
-        Axios.get("./api/proveedores/"+ proveedorByName).then((res)=>{
-            setProveedores(res.data)
-            //console.log(res.data)
-        }
-        )
-    }    
-    
-    }
 
     const searchProveedores = async ()=>{
         Axios.get("./api/proveedores/"+ proveedorByName).then((res)=>{
@@ -46,7 +36,7 @@ export default function Admin(){
 
         Axios.delete("./api/proveedor/" + id).then((res) => {
             alert(res.data.message)
-            getProveedores()
+            searchProveedores()
         }).catch(err => {
             alert(err.data.message)
             alert("error")
@@ -59,7 +49,7 @@ export default function Admin(){
             
             <h1 className="titulo">Lista de Proveedores</h1>
             <label htmlFor="filtro">Nombre</label>
-            <input type="text" id="filtro" onChange={(e)=>{ getProveedorByName(e)}} />
+            <input type="text" id="filtro" onChange={(e)=>{ setProveedorByName(e)}} />
             <Link to="/Provedores" className="boton-crear-usuario">Agregar Proveedor</Link>
             <table id="customers">
                 <tr>
@@ -91,3 +81,5 @@ export default function Admin(){
 
         </>
     )
+
+                }
