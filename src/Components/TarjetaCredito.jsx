5190b166da662c6b852ccbe6649e8d9667f7ec7b
Luis Vargas <72893367+luisv99@users.noreply.github.com>
2021-06-18 11:42:49 -0400

    luis arreglando pago TDC

--- src/Components/TarjetaCredito.jsx
+++ src/Components/TarjetaCredito.jsx
@@ -6,34 +6,34 @@ export default function TarjetaCredito (){
 
     return(
         <>
-            <div className="cont">
-            <div className="signupSection">
-                <div className="info">
+            <div className="contCredito">
+            <div className="signupSectionCredito">
+                <div className="infoCredito">
                     <h1>E-Vent</h1>
                     <br></br>
                     <h1>LOGO</h1>
                 </div>
                 
-                <form action="#"   className="signupForm" name="signupform">
+                <form action="#"   className="signupFormCredito" name="signupform">
                     <h2>Tarjeta De Credito</h2>
                     <ul className="noBulletCredito">
                         <li>
                             <label htmlfor="Nombre"></label>
-                            <input type="cc-given-name" className="inputFields" id="nombre" name="nombre" placeholder="Nombre"  />
+                            <input type="cc-given-name" className="inputFieldsCredito" id="nombre" name="nombre" placeholder="Nombre"  />
                         </li>
                     
                         <li>
                             <label htmlfor="Número de Tarjeta"></label>
-                            <input type="cardNumber" className="inputFields" id="location" name="numTarjeta" placeholder="Número De Tarjeta"  />
+                            <input type="cardNumber" className="inputFieldsCredito" id="location" name="numTarjeta" placeholder="Número De Tarjeta"  />
                         </li>
                         <li>
                             <label htmlfor="Numero de personas"></label>
-                            <input type="cc-csc" maxLength="3"  className="inputFields" id="number" name="number" placeholder="CVV"   />
+                            <input type="cc-csc" maxLength="3"  className="inputFieldsCredito" id="number" name="number" placeholder="CVV"   />
                         </li>
 
                         <li>
                             <label htmlfor="Fecha del evento"></label>
-                            <input type="month" className="inputFields" id="date" name="date" placeholder="número de Personas"  />
+                            <input type="month" className="inputFieldsCredito" id="date" name="date" placeholder="número de Personas"  />
                         </li>
                         <li id="center-btn">
                             <input type="submit" id="join-btn" name="join" alt="Join" value="Pagar E-vent"/>
